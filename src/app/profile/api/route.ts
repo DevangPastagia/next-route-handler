11c9ee0bf9e1cc6c7daced3bc34af6d9fac67e17
DevangPastagia <devang.pastagia@girirajdigital.com>
2025-05-13 17:30:08 +0530

    feat: handle cookies both methods(formats)

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,16 +1,21 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headerList = await headers();
 
-  // Get cookies
+  // Get cookies - first method
   const theme = request.cookies.get("theme");
+
+  // Get cookies - second method
+  const cookie = await cookies();
+  cookie.set("page", "3");
 
   console.log("Request Headers:", requestHeaders.get("Authorization"));
   console.log("Header List:", headerList.get("Authorization"));
   console.log("Theme Cookie:", theme);
+  console.log("Cookie:", cookie.get("page"));
 
   return new Response("<h1>Hello from the profile API</h1>", {
     headers: {
