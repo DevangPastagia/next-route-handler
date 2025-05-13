d589185f9e41370117246b264031929a88127c7a
DevangPastagia <devang.pastagia@girirajdigital.com>
2025-05-13 16:52:28 +0530

    feat: handle patch and delete request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,12 +1,45 @@
 import { comments } from "../faker/data";
 
-export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } }
-) {
+type props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(_request: Request, { params }: props) {
   const { id } = params;
   const comment = comments.find((comment) => comment.id.toString() === id);
   return comment
     ? Response.json(comment)
     : Response.json({ error: "Comment not found" }, { status: 404 });
+}
+
+export async function PATCH(request: Request, { params }: props) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((comment) => comment.id.toString() === id);
+
+  comments[index] = {
+    ...comments[index],
+    text,
+  };
+
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(_request: Request, { params }: props) {
+  const { id } = await params;
+  let deletedComment = {};
+  const index = comments.findIndex((comment) => comment.id.toString() === id);
+  deletedComment = comments[index];
+
+  if (index === -1) {
+    return Response.json({ error: "Comment not found" }, { status: 404 });
+  }
+
+  comments.splice(index, 1);
+
+  return Response.json(deletedComment, { status: 200 });
 }
